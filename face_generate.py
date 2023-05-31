dcfc88426f8e7e080b065425fd4bccef16d09050
gunesaltiner <gunes.altiner@ozu.edu.tr>
2023-05-31 19:28:02 +0300

    fixed

--- face_generate.py
+++ face_generate.py
@@ -1,4 +1,3 @@
-
 # import the necessary packages
 from imutils import face_utils
 import numpy as np
@@ -11,76 +10,55 @@ import _pickle as cPickle
 
 import os
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the (optional) video file")
-args = vars(ap.parse_args())
+argp = argparse.ArgumentParser()
+argp.add_argument("-v", "--video", help="path to the (optional) video file")
+args = vars(argp.parse_args())
 
-number=0;
-frame_count=0
-detector = dlib.get_frontal_face_detector()
+numb = 0;
+f_count = 0
+detect = dlib.get_frontal_face_detector()
 print("enter the person name")
 name = input()
-folder_name="dataset/"+name
+name_file = "dataset/" + name
 
-if os.path.exists(folder_name):
-    print ("Folder exist")
+if os.path.exists(name_file):
+    print("Folder exist")
 else:
-	os.mkdir(folder_name)
+    os.mkdir(name_file)
 
-# if a video path was not supplied, grab the reference to the webcam
 if not args.get("video", False):
-	
-	camera = cv2.VideoCapture(0)
- 
-# otherwise, grab a reference to the video file
-else:
-	camera = cv2.VideoCapture(args["video"])
+    cam = cv2.VideoCapture(0)
 
+else:
+    cam = cv2.VideoCapture(args["video"])
 
 while True:
-	
-	if frame_count % 5 == 0:
+    if f_count % 5 == 0:
+        print("keyframe")
+        (grabbed, picture) = cam.read()
 
-		print("keyframe")
+        if args.get("video") and not grabbed:
+            break
+        picture = imutils.resize(picture, width=500)
+        gr = cv2.cvtColor(picture, cv2.COLOR_BGR2GRAY)
+        rects = detect(gr, 1)
+        for (i, rect) in enumerate(rects):
+            (x, y, w, h) = face_utils.rect_to_bb(rect)
+            cro = picture[y: y + h, x: x + w]
+            picture_out = cv2.resize(cro, (108, 108))
+            fr = os.path.join(name_file + "/", str(numb) + "." + "jpg")
+            numb = numb + 1
+            cv2.imwrite(fr, picture_out)
+            cv2.rectangle(picture, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        f_count = f_count + 1
+    else:
+        f_count = f_count + 1
+        print("redundant frame")
+    if numb > 51:
+        break
+    cv2.imshow("output image", picture)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-		# grab the current frame
-		(grabbed, image) = camera.read()
-		# if we are viewing a video and we did not grab a
-		# frame, then we have reached the end of the video
-		if args.get("video") and not grabbed:
-			break
-		image = imutils.resize(image, width=500)
-		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-			# detect faces in the grayscale image
-		rects = detector(gray, 1)
-		# loop over the face detections
-		for (i, rect) in enumerate(rects):
-			# determine the facial landmarks for the face region, then			
-			(x, y, w, h) = face_utils.rect_to_bb(rect)
-			#print rect.dtype
-			cro=image[y: y + h, x: x + w]
-
-			out_image = cv2.resize(cro,(108,108))
-			
-			fram= os.path.join(folder_name+"/",str(number)+ "." + "jpg")
-			number+=1		
-			cv2.imwrite(fram,out_image)		
-			cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)			
-		frame_count+=1
-		
-	else:
-
-		frame_count+=1
-		print("redudant frame")
-
-	if number >51:
-		break			
-	#cv2.imshow("output", image)	
-	cv2.imshow("output image",image)	
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-
-# clean up the camera and close any open windows
-camera.release()
+cam.release()
 cv2.destroyAllWindows()
--- face_train.py
+++ face_train.py
@@ -12,51 +12,30 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
-   
-    # The training data would be all the face encodings from all the known images and the labels are their names
-    encodings = []
-    names = []
-
-    # Training directory
-
+    encodes = []
+    all_names = []
     train_dir = os.listdir('dataset/')
     print(train_dir)
-
-    # Loop through each person in the training directory
     for person in train_dir:
-        pix = os.listdir("dataset/" + person)
-    
-        # Loop through each training image for the current person
-        for person_img in pix:
-            # Get the face encodings for the face in each image file
-            print("dataset/" + person + "/" + person_img)
-            face = face_recognition.load_image_file("dataset/" + person + "/" + person_img)
-            print(face.shape)
-            # Assume the whole image is the location of the face
-            #height, width, _ = face.shape
-            # location is in css order - top, right, bottom, left
-            height, width, _ = face.shape
-            face_location = (0, width, height, 0)
-            print(width,height)
-
-            face_enc = face_recognition.face_encodings(face, known_face_locations=[face_location])
-            
-            
-
+        p = os.listdir("dataset/" + person)
+        for picture_human in p:
+            print("dataset/" + person + "/" + picture_human)
+            front = face_recognition.load_image_file("dataset/" + person + "/" + picture_human)
+            print(front.shape)
+            h, w, _ = front.shape
+            face_location = (0, w, h, 0)
+            print(w, h)
+            face_enc = face_recognition.face_encodings(front, known_face_locations=[face_location])
             face_enc = np.array(face_enc)
             face_enc = face_enc.flatten()
         
-            # Add face encoding for current image with corresponding label (name) to the training data
-            encodings.append(face_enc)
-            names.append(person)
-
-    print(np.array(encodings).shape)
-    print(np.array(names).shape)
-    # Create and train the KNN classifier
+            encodes.append(face_enc)
+            all_names.append(person)
+    print(np.array(encodes).shape)
+    print(np.array(all_names).shape)
     knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
-    knn_clf.fit(encodings,names)
+    knn_clf.fit(encodes,all_names)
 
-    # Save the trained KNN classifier
     if model_save_path is not None:
         with open(model_save_path, 'wb') as f:
             pickle.dump(knn_clf, f)
@@ -64,10 +43,7 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
     return knn_clf
 
 
-
 if __name__ == "__main__":
-    # STEP 1: Train the KNN classifier and save it to disk
-    # Once the model is trained and saved, you can skip this step next time.
     print("Training KNN classifier...")
     classifier = train("dataset", model_save_path="trained_knn_model.clf", n_neighbors=2)
     print("Training complete!")    
